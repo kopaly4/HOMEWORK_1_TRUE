eb6a785cce2f4d2a81ca36fa9a30c7b956e6b0e4
kopaly4 <otrafdgb@mail.ru>
2021-07-02 00:20:55 +0300

    homework_4_X0game

---
+++ src/homeworkapp4/homeworkapp4.java
@@ -0,0 +1,211 @@
+package homeworkapp4;
+
+import com.sun.glass.ui.Size;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class homeworkapp4 {
+    public static char map[][];
+    public static final int SIZE = 5;
+    public static final char DOT_X = 'X';
+    public static final char DOT_O = 'O';
+    public static final char DOT_EMPTY = '.';
+    public static final int DOTS_TO_WIN = 4;
+    static Scanner sc = new Scanner(System.in);
+    static Random random = new Random();
+
+    public static void main(String[] args) {
+        initMap();
+        printMap();
+        while (true) {
+            humanTurn();
+            printMap();
+            if (checkWin(DOT_X)) {
+                System.out.println("Вы победили!");
+                break;
+            }
+            if (isFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+            aiTurn();
+            printMap();
+            if (checkWin(DOT_O)) {
+                System.out.println("Компьютер победил!");
+                break;
+            }
+            if (isFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+        }
+
+    }
+
+    public static void initMap() {
+        map = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                map[i][j] = DOT_EMPTY;
+
+            }
+
+        }
+    }
+
+    public static void printMap() {
+        System.out.print("  ");
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print(i + 1 + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print(i + 1 + " ");
+
+            for (int j = 0; j < SIZE; j++) {
+                System.out.printf("%c ", map[i][j]);
+            }
+            System.out.println();
+
+        }
+
+    }
+
+    public static void humanTurn() {
+        int x, y;
+        do {
+            System.out.println("Input Y,X");
+            y = sc.nextInt() - 1;
+            x = sc.nextInt() - 1;
+        } while (!isCellValid(y, x));
+        map[y][x] = DOT_X;
+    }
+
+    public static void aiTurn() {
+        int x, y;
+        do {
+            y = random.nextInt(SIZE);
+            x = random.nextInt(SIZE);
+        } while (!isCellValid(y, x));
+        map[y][x] = DOT_O;
+    }
+
+    public static boolean isCellValid(int y, int x) {
+        if (x < 0 || y < 0 || x >= SIZE || y >= SIZE) {
+            return false;
+        }
+        return map[y][x] == DOT_EMPTY;
+
+
+    }
+
+    public static boolean isFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOT_EMPTY) {
+                    return false;
+                }
+
+            }
+        }
+        return true;
+
+    }
+
+    public static boolean checkWin(char c) {
+        boolean check = false;
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (CheckWinParallelLine(i, j, c) || CheckWinPerpendicularLine(i, j, c) || CheckWinDiagonalLine(i, j, c) || CheckWinDiagonalLine_2(i, j, c)) {
+                    check = true;
+                } else continue;
+
+            }
+        }
+        return check;
+    }
+
+    public static boolean CheckWinParallelLine(int y_, int x_, char dot) {
+        int counter = 0;
+        if (x_ + DOTS_TO_WIN > SIZE) {
+            return false;
+        }
+        for (int i = 0; i < DOTS_TO_WIN; i++) {
+            if (map[y_][x_ + i] == dot) {
+                counter++;
+            }
+        }
+        return (counter == DOTS_TO_WIN);
+    }
+
+    public static boolean CheckWinPerpendicularLine(int y_, int x_, char dot) {
+        int counter = 0;
+        if (y_ + DOTS_TO_WIN > SIZE) {
+            return false;
+        }
+        for (int i = 0; i < DOTS_TO_WIN; i++) {
+
+            if (map[y_ + i][x_] == dot) {
+                counter++;
+            }
+        }
+        return (counter == DOTS_TO_WIN);
+    }
+
+    public static boolean CheckWinDiagonalLine(int y_, int x_, char dot) {
+        int counter = 0;
+        if (y_ + DOTS_TO_WIN > SIZE || x_ + DOTS_TO_WIN > SIZE) {
+            return false;
+        }
+        for (int i = 0; i < DOTS_TO_WIN; i++) {
+
+            if (map[y_ + i][x_ + i] == dot) {
+                counter++;
+            }
+        }
+        return (counter == DOTS_TO_WIN);
+    }
+
+    public static boolean CheckWinDiagonalLine_2(int y_, int x_, char dot) {
+        int counter = 0;
+        if (y_ - DOTS_TO_WIN + 1 < 0 || x_ + DOTS_TO_WIN > SIZE) {
+            return false;
+        }
+        for (int i = 0; i < DOTS_TO_WIN; i++) {
+
+            if (map[y_ - i][x_ + i] == dot) {
+                counter++;
+            }
+        }
+        return (counter == DOTS_TO_WIN);
+    }
+}
+//    public static boolean checkWin(char c) {
+//        if (map[0][0] == c && map[0][1] == c && map[0][2] == c) {
+//            return true;
+//        } ;
+//        if (map[1][0] == c && map[1][1] == c && map[1][2] == c) {
+//            return true;
+//        } ;
+//        if (map[2][0] == c && map[2][1] == c && map[2][2] == c) {
+//            return true;
+//        }
+//        if (map[0][0] == c && map[1][0] == c && map[2][0] == c) {
+//            return true;
+//        }
+//        if (map[0][1] == c && map[1][1] == c && map[2][1] == c) {
+//            return true;
+//        }
+//        if (map[0][2] == c && map[1][2] == c && map[2][2] == c) {
+//            return true;
+//        }
+//        if (map[0][0] == c && map[1][1] == c && map[2][2] == c) {
+//            return true;
+//        }
+//        if (map[0][2] == c && map[1][1] == c && map[2][0] == c) {
+//            return true;
+//        }
+//        return false;
+//    }
+
