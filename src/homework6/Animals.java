309f405b08cdb03ea47e5b7b8f43af2a82342fd9
kopaly4 <otrafdgb@mail.ru>
2021-07-06 14:52:21 +0300

    homework_6

---
+++ src/homework6/Animals.java
@@ -0,0 +1,10 @@
+package homework6;
+
+public abstract class Animals {
+    String name;
+    static int animalcounter;
+
+    public abstract void run(int a);
+
+    public abstract void swim(int b);
+}
